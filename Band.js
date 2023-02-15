fc52fd2283df962876a24fb9d147aa9b8bd1ac22
Tyler <tyler.mackay@barclays.com>
2023-02-15 15:29:29 +0000

    Updated connection, added band and musician modles

--- Band.js
+++ Band.js
@@ -1,7 +1,11 @@
 const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Band model
-let Band;
+const {DataTypes} = require("sequelize");
+let Band = sequelize.define("Band", {
+    name: DataTypes.STRING,
+    genre: DataTypes.STRING,
+   });
 
 module.exports = {
     Band
--- Musician.js
+++ Musician.js
@@ -1,7 +1,12 @@
 const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Musician model
-let Musician;
+const {DataTypes} = require("sequelize");
+let Musician = sequelize.define("Band", {
+    name: DataTypes.STRING,
+    instrument: DataTypes.STRING,
+   });
+
 
 module.exports = {
     Musician
